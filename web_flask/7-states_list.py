2f94ff8b9ec69da7c1b60d52e55c3d35e64352b2
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-07-09 23:38:53 +0000

    final form.

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,71 @@
 #!/usr/bin/python3
-"""Importing Flask to run the web app"""
+""" task 7 """
 from flask import Flask, render_template
-from models import storage
-from models.state import State
 
 
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def display_states():
-    """Render state_list html page to display States created"""
-    states = storage.all()
-    return render_template('7-states_list.html', states=states)
+@app.route('/', strict_slashes=False)
+def root():
+    return "Hello HBNB!"
+
+
+@app.route('/hnbn', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    return f"C {text.replace('_', '')}"
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return f"{n} is a number"
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n: int):
+    if n % 2 == 0:
+        value = 'even'
+    else:
+        value = 'odd'
+    return render_template('6-number_odd_or_even.html', n=n, value=value)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    from models import storage
+    from models.state import State
+
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('7-states_list.html', states=sorted_states)
+
+
+def get_name(item):
+    key, value = item
+    return value.name
 
 
 @app.teardown_appcontext
-def teardown(self):
-    """Method to remove current SQLAlchemy Session"""
+def teardown(exc):
+    from models import storage
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
