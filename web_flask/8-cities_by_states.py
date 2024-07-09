8cba51dacc29602a6353f4a5669cfcc1d2e2a9d5
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-07-09 22:14:06 +0000

    updated 8 for both routes

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -15,10 +15,13 @@ def close(self):
 
 
 @app.route('/cities_by_states', strict_slashes=False)
-def cities_by_states():
-    """Displays a html page with states and cities"""
-    states = storage.all(State)
-    return render_template('8-cities_by_states.html', states=states)
+def states_list():
+    """ returns a list of cities by state """
+    states = storage.all(State).values()
+    for state in states:
+        state.cities.sort(key=lambda city: city.name)
+    sorted_states = sorted(states, key=lambda state: state.name)
+    return render_template('8-cities_by_states.html', states=sorted_states)
 
 
 if __name__ == '__main__':
