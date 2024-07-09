f521112bc5226b83e5dbbdd4e81ba5fc3a19faa0
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-07-09 22:52:24 +0000

    revert again?

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,46 +1,25 @@
 #!/usr/bin/python3
-""" task 9 """
+"""Importing Flask to run the web app"""
 from flask import Flask, render_template
+from models import storage
+from models.state import State
 
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def root():
-    return "This Project Sucks!"
-
-
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    from models import storage
-    from models.state import State
-
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=sorted_states)
+@app.teardown_appcontext
+def close(self):
+    """ Method to close the session """
+    storage.close()
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    from models import storage
-    from models.state import State
-    from models.city import City
-
-    states = storage.all(State).values()
-    sorted_states = sorted(states, key=lambda state: state.name)
-    cities = storage.all(City).values()
-    sorted_cities = sorted(cities, key=lambda city: city.name)
-    return render_template('8-cities_by_states.html',
-                           states=sorted_states,
-                           cities=sorted_cities)
+    """Displays a html page with states and cities"""
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
 
 
-@app.teardown_appcontext
-def teardown(exc):
-    from models import storage
-    storage.close()
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="5000")
