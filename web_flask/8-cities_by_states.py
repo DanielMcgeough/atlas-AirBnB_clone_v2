51bfdd67cb1b808fd7e26fa534315bd7dde096c6
DanielMcgeough <daniel.mcgeough@atlasschool.com>
2024-07-09 22:22:42 +0000

    oops.

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -8,42 +8,7 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def root():
-    return "Hello HBNB!"
-
-
-@app.route('/hnbn', strict_slashes=False)
-def hbnb():
-    return "HBNB"
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    return f"C {text.replace('_', '')}"
-
-
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python(text="is cool"):
-    return f"Python {text.replace('_', ' ')}"
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    return f"{n} is a number"
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    return render_template('5-number.html', n=n)
-
-
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_odd_or_even(n: int):
-    if n % 2 == 0:
-        value = 'even'
-    else:
-        value = 'odd'
-    return render_template('6-number_odd_or_even.html', n=n, value=value)
+    return "This Project Sucks!"
 
 
 @app.route('/states_list', strict_slashes=False)
